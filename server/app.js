5c0f1e9987061b27e1a4d683aeecbdae4947561a
doctor <fd5@MacBook-Air-2022.local>
2024-01-15 14:42:07 +0700

    express

--- server/app.js
+++ server/app.js
@@ -1,24 +1,35 @@
+import { client } from "./utils/db.js";
 import express from "express";
 import cors from "cors";
 import productRouter from "./apps/products.js";
 
-const app = express();
-const port = 4001;
-
 // `cors` เป็น Middleware ที่ทำให้ Client ใดๆ ตามที่กำหนด
 // สามารถสร้าง Request มาหา Server เราได้
 // ในโค้ดบรรทัดล่างนี้คือให้ Client ไหนก็ได้สามารถสร้าง Request มาหา Server ได้
-app.use(cors());
+async function init() {
+  try {
+    // เชื่อมต่อ database
+    await client.connect();
+  } catch {
+    console.log("cannot connect to the server");
+  }
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+  const app = express();
 
-app.use("/products", productRouter);
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
+  app.use(cors());
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+  const port = 4001;
 
-app.listen(port, () => {
-  console.log(`Server is running at port ${port}`);
-});
+  app.use("/products", productRouter);
+
+  app.get("/", (req, res) => {
+    res.send("Hello World! ");
+  });
+
+  app.listen(port, () => {
+    console.log(`Server is running at port ${port}`);
+  });
+}
+init();
--- server/utils/db.js
+++ server/utils/db.js
@@ -1,1 +1,7 @@
 // Set up db connection here
+import { MongoClient } from "mongodb";
+const url = "mongodb://localhost:27017";
+const dbName = "practice-mongo";
+
+export const client = new MongoClient(url);
+export const db = client.db(dbName);
